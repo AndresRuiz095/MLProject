74b5d66a30b036df250192e66451f2689d4d1520
AndresRuiz095 <andresruuiz95@gmail.com>
2025-05-09 13:37:01 -0600

    Final proyect

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd 
 from src.exception import CustomException
 import dill
+import pickle
 
 from sklearn.model_selection import GridSearchCV
 
@@ -50,3 +51,11 @@ def evaluate_models(X_train,X_test,y_train,y_test,models,params,cv=3,n_jobs=1,ve
             return report
     except Exception as e:
         raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
